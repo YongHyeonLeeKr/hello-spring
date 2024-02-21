09acfa84b6b385f528f8ca28bfc7b55cd3e2d731
noahLee <yonghyeon.lee.kr@gmail.com>
2024-02-21 12:07:38 +0900

    [스프링빈과의존관계]홈화면 주가

---
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -0,0 +1,26 @@
+package hello.hellospring;
+
+
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 자바 코드로 직접 스피링 빈 등록
+ */
+
+@Configuration
+public class SpringConfig {
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+}
